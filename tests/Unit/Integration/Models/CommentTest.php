c1d6f676e3d3027a4c81ef9b9f4dc38560ea93bd
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2019-12-06 23:11:20 +0200

    refector code in setup for tests

--- tests/Unit/Integration/Models/CommentTest.php
+++ tests/Unit/Integration/Models/CommentTest.php
@@ -8,45 +8,30 @@ use Tests\TestCase;
 class CommentTest extends TestCase
 {
 
+    private $comment;
+
     public function setUp(): void
     {
         parent::setUp();
 
+        factory(Comment::class, 3)->create();
+
+        factory(Comment::class, 4)->state('approved')->create();
+
+        $this->comment = new Comment;
+
     }
 
-    // setup
     /** @test */
     public function it_should_render_approved_comments_only()
     {
-
-        factory(Comment::class, 3)->create();
-
-        factory(Comment::class, 4)->state('approved')->create();
-
-        $comment = new Comment;
-
-        $this->assertCount(4, $comment->approved()->get());
+        $this->assertCount(4, $this->comment->approved()->get());
 
     }
-    // teardown
 
-    //setup
     /** @test */
     public function it_should_render_disapproved_comments_only()
     {
-
-        factory(Comment::class, 3)->create();
-
-        factory(Comment::class, 4)->state('approved')->create();
-
-        $comment = new Comment;
-
-        //$this->assertCount(3, $comment->approved(false)->get());
-
-        $this->assertCount(3, $comment->disapproved()->get());
-
-        //$this->assertCount(3, $comment->disapproved()->recentlyCreated()->get());
-
+        $this->assertCount(3, $this->comment->disapproved()->get());
     }
-    //teardown
 }
--- tests/Unit/Integration/Models/PostTest.php
+++ tests/Unit/Integration/Models/PostTest.php
@@ -7,46 +7,32 @@ use Tests\TestCase;
 
 class PostTest extends TestCase
 {
+    private $post;
 
     public function setUp(): void
     {
         parent::setUp();
 
+        factory(Post::class, 3)->create();
+
+        factory(Post::class, 4)->state('approved')->create();
+
+        $this->post = new Post;
+
     }
 
-    // setup
     /** @test */
     public function it_should_render_approved_posts_only()
     {
 
-        factory(Post::class, 3)->create();
-
-        factory(Post::class, 4)->state('approved')->create();
-
-        $post = new Post;
-
-        $this->assertCount(4, $post->approved()->get());
+        $this->assertCount(4, $this->post->approved()->get());
 
     }
-    // teardown
 
-    //setup
     /** @test */
     public function it_should_render_disapproved_posts_only()
     {
-
-        factory(Post::class, 3)->create();
-
-        factory(Post::class, 4)->state('approved')->create();
-
-        $post = new Post;
-
-        //$this->assertCount(3, $post->approved(false)->get());
-
-        $this->assertCount(3, $post->disapproved()->get());
-
-        //$this->assertCount(3, $post->disapproved()->recentlyCreated()->get());
+        $this->assertCount(3, $this->post->disapproved()->get());
 
     }
-    //teardown
 }
