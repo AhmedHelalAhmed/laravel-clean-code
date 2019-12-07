be807336b83d7793bb2a9d00a65528b35399b5eb
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2019-12-07 16:19:21 +0200

    Controller the event queue and put the event in it or not

--- app/listeners/SendEmailVerificationNotification.php
+++ app/listeners/SendEmailVerificationNotification.php
@@ -4,12 +4,28 @@ namespace App\listeners;
 
 use App\Events\UserWasRegistered;
 use App\Mail\ActivationSent;
+use Illuminate\Bus\Queueable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Mail;
 
 class SendEmailVerificationNotification implements ShouldQueue
 {
+
+
+    use InteractsWithQueue, Queueable;
+
+    // do not consume the queue if you do not need it
+
+    public function sholudQueue($event)
+    {
+
+
+        // dd($event); // based on the information of the event
+        // return false;// to make the event not dispatched to the queue
+
+        return true;// to allow event to work and enter the queue
+    }
 
 
     /**
